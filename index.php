b4313773579214b5d5b869a02f1a2eed0c89dc40
Gianfranco Navas Fernandini <gianf2605@gmail.com>
2023-11-28 13:56:46 +0100

    grid blog page

--- index.php
+++ index.php
@@ -8,9 +8,94 @@
 get_header();
 
 ?>
-<div class="content">content</div>
+
+<div id="primary">
+
+    <main id="main" class="site-main mt-5" role="main">
+
+    <?php
+        if ( have_posts() ) {
+
+            ?>
+            
+            <div class="container">
+
+                <?php
+                    // check if i'm in the homepage but not in the front-page
+                    if( is_home() && ! is_front_page() ) {
+
+                        ?>
+
+                        <header class="mb-5">
+
+                            <h1 class="page-title">
+
+                                <?php single_post_title(); ?>
+
+                            </h1>
+
+                        </header>
+
+                        <?php
+                    }
+
+                    // <!-- Variables for the columns -->
+                    $index = 0;
+                    $num_of_columns = 3;
+
+                    while ( have_posts() ) : the_post();
+
+                        if ( 0 === $index % $num_of_columns ) {
+
+                            ?>
+
+                            <div class="row">
+
+                            <?php
+                        }
+
+                        ?>
+
+                        <div class="col-lg-4 col-md-6 col-sm-12">
+
+                        <h3><?php the_title(); ?></h3>
+
+                            <p><?php the_excerpt(); ?></p>
+
+                        </div>
+                           
+
+                        <?php
+
+                        $index++;
+
+                        if ( 0 !== $index && 0 ===  $index % $num_of_columns) {
+
+                            ?>
+
+                            </div>
+
+                            <?php
+
+                        }
+
+                    endwhile;
+
+                    ?>
+
+            </div>
+
+            <?php
+
+        }
+
+    ?>
+
+    </main>
+
+</div>
 
 <?php
 
-get_footer();
+get_footer(); 
 
---
+++ single-post.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Single Post Template 
+ * 
+ * @package aquila
+*/
+
+get_header();
+
+?>
+
+    <div>Single Post content</div>
+
+<?php
+
+
+get_footer();
+
+
+
